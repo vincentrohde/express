d011e7e81db4bc23f090aa4d079a61d6d0c0f870
Vincent Rohde <vincent.rohde@icloud.com>
2018-01-25 15:47:06 +0100

    Add post magic

--- 1-contact/app.js
+++ 1-contact/app.js
@@ -18,10 +18,10 @@ app.get('/', function(req,  res) {
   res.render('contact');
 });
 
-app.post('/contact', urlencodedParser, function (req, res) {
+app.post('/', urlencodedParser, function (req, res) {
   res.render('contact-success', {
     data: req.body
-  });;
+  });
 });
 
 app.listen(3000);
